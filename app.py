1ec545751f654c494a1d6a7a4d159e8cebf95be7
CHAEYULL <leopportunity5@gmail.com>
2024-03-10 15:20:36 +0900

    파일 생성

---
+++ app.py
@@ -0,0 +1,33 @@
+# 파일 = open('text.txt','w') #open('작명', 'w) 작성하고 싶으면 w
+# 파일.write('헬로') #작성하고싶으면 파일.write
+# 파일.close() #저장하기
+
+# 파일 = open('text.txt','r') #r은 읽기 모드임 파일안에 적힌거 다 볼수있음
+# print(파일.read()) #파일 안에 내용 읽기
+# 파일.close()
+
+# 파일 = open('text.txt','a')#a 추가모드임
+# 파일.write('\n반가워')
+# 파일.close()
+
+# 파일 = open('text.txt','r') #r은 읽기 모드임 파일안에 적힌거 다 볼수있음
+# print(파일.read()) #파일 안에 내용 읽기
+# 파일.close()
+
+
+#엑셀 형식 파일 만들기
+
+# file = open('data.csv', 'w')
+# file.write('kim, lee, park')
+# file.close()
+
+# list = ['samsung', 'kakao','naver', 'kwangdong']
+
+# f = open('data.txt','w')
+# for i in list:
+#     f.write( i + '\n')
+# f.close()
+
+
+
+        
---
+++ crawler.py
@@ -0,0 +1,6 @@
+#pip3 install requests
+#sudo pip install requests
+#sudo pip3 install requests
+import requests
+#pip install bs4
+from bs4 import BeautifulSoup
