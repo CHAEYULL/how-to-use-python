ca0317b8583e15e29118c6edff461643d6a9854a
CHAEYULL <leopportunity5@gmail.com>
2024-03-11 12:39:43 +0900

    크롤링(시간 포메팅 json크롤링)

--- crawler.py
+++ crawler.py
@@ -4,3 +4,14 @@
 import requests
 #pip install bs4
 from bs4 import BeautifulSoup
+import json
+import time
+
+data = requests.get('https://api-gateway.coinone.co.kr/exchange/chart/v1/KRW/ETH?lastDt=1689249600000&interval=1H&1710126620947')
+
+딕셔너리 = json.loads(data.content)
+for i in range(100):
+    시간 = 딕셔너리['body']['candles'][i]['dt']
+    realTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(시간/1000)) 
+    print(realTime)
+
